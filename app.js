770a96d3dc5a576f3f0458d43b9045b3fc41b5b8
Alexandre Dutier <alex.dutier@ibm.com>
2024-12-17 10:35:52 +0000

    change socket port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const expressLayouts = require('express-ejs-layouts');
 const { WebSocketServer } = require('ws');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // View engine and layouts
 app.set('view engine', 'ejs');
