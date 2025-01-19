e71edd3b7d183ddf1122a2110fc0271a85543e4e
alex2t <your_noreply_email@users.noreply.github.com>
2025-01-19 02:31:55 +0000

    change port in app js

--- app.js
+++ app.js
@@ -31,8 +31,9 @@ app.locals.broadcastEvent = (event) => {
 };
 
 
-// Start server
-const PORT = 3001;
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3001;
+const HOST = process.env.HOST || 'localhost'; // Use 'localhost' for local development, and '0.0.0.0' for production
+
+server.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
